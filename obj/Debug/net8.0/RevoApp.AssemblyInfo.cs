03e920ba48448a9b419bfd58340716e021c7bb37
SeIectra <ardaguner2000@gmail.com>
2025-07-06 21:47:35 +0300

    Initial commit

--- obj/Debug/net8.0/RevoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/RevoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RevoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78c1cb2236d58d1a2c8c93f3e1afcbe36aec1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RevoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RevoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
